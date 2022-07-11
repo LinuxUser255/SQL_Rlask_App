bd092bc2b4723b5a354bfce806e0b2e04789d4e7
Chris <atnl@protonmail.com>
2022-07-11 14:18:36 -0400

    Create url_fetch.py

---
+++ Other/url_fetch.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import urllib
+import os
+from flask import Flask, redirect, request
+from secrets import token_hex
+
+app = Flask(__name__)
+
+
+@app.route('/fetch')
+def fetch():
+    url = request.args.get('url', '')
+    if url.startswith("https://insecurewebapp.com"):
+        response = urllib.request.urlopen(url)
+        html = response.read()
+        return html
+    return ""
