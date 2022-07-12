e75dedbc592d62448b8164e1e325c69ff79ebcdf
Chris <atnl@protonmail.com>
2022-07-12 17:30:26 -0400

    Update app.py

--- XSS/app.py
+++ XSS/app.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env/python 3
+
+"""
+app.py
+
+SYNOPSIS
+========
+::
+ 
+   Follow the set up instructions on the README.md
+   After executing export FLASK_ENV=development
+   A server will be running on localhost port 5000
+   Open your browser and navigate to: http://127.0.0.1:5000
+   The Web App should open and you can begin testing
+   This process is the same for running each Flasjk App in this repository.
+
+DESCRIPTION
+===========
+
+This is an XSS vulnerable web app. 
+See the commented lines of source code in index.html for specific examples.
+The code in index.html is intended to be modified to demonstrate reflected and stored XSS
+
+"""
 from flask import Flask, render_template, request
 import db
 
